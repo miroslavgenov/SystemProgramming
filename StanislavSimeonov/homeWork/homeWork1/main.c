9b23d32466747c7db6c50c2b8db330e1e3422634
miroslavgenov <buchvata123@abv.bg>
2023-04-22 21:33:14 +0300

    homework1 init

---
+++ StanislavSimeonov/homeWork/homeWork1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void){
+
+    return EXIT_SUCCESS;
+}
---
+++ StanislavSimeonov/homeWork/homeWork1/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash env bash
+
+make
+./main
+make clean
