326afa65ec03bae31b55c7b6aa5cafb2ba054640
miroslavgenov <buchvata123@abv.bg>
2023-04-23 19:47:25 +0300

    homework 1 test solv

--- StanislavSimeonov/homeWork/homeWork1/main.c
+++ StanislavSimeonov/homeWork/homeWork1/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
+
+void printProcessInformation(){
+    printf("\nCurrent PID: %d\n",getpid());
+    printf("The Parent Pid of the Current Pid: %d\n",getppid());
+}
 
 int main(void){
 
+    for(size_t i = 0; i < 2; i++){
+        fork();
+        printProcessInformation();
+    }    
+    
     return EXIT_SUCCESS;
 }
--- StanislavSimeonov/homeWork/homeWork1/run.sh
+++ StanislavSimeonov/homeWork/homeWork1/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash env bash
-
 make
 ./main
+
 make clean
